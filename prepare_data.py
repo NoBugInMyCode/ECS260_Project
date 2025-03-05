a0c5898d9d585881b7d3f773b55edd34da9fce97
JayChao0331 <45892063+JayChao0331@users.noreply.github.com>
2025-03-04 18:28:31 -0800

    Add files via upload

--- prepare_data.py
+++ prepare_data.py
@@ -23,14 +23,19 @@ def prepare_data():
     X = df_cleaned[input_columns]
     y = df_cleaned[target_column]
 
+    # Split the data into training and testing sets with a fixed random state
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Normalize using only training set statistics
     dtypes = list(zip(X.dtypes.index, map(str, X.dtypes)))
     for k, dtype in dtypes:
         if dtype == "float32":
-            X.loc[:, k] -= X[k].mean()
-            X.loc[:, k] /= X[k].std()
+            mean = X_train[k].mean()
+            std = X_train[k].std()
 
-    # Split the data into training and testing sets with a fixed random state
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            # Apply normalization
+            X_train.loc[:, k] = (X_train[k] - mean) / std
+            X_test.loc[:, k] = (X_test[k] - mean) / std
 
     print("X_train: {}".format(X_train.shape))
     print("y_train: {}".format(y_train.shape))
